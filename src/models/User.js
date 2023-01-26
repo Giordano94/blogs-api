f0901887dd7b3dce09efe7a7f575c1b5460941f3
Giordano <giordano.mendes94@gmail.com>
2023-01-26 19:16:26 -0300

    feat: model User

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId',
+      as: 'blogPosts',
+    });
+  };
+
+  return User;
+};
+
+module.exports = User;
