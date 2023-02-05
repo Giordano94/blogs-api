0f5ae011f50fe97b1b933cffb5956c0c8c2f63dd
Giordano <giordano.mendes94@gmail.com>
2023-02-05 16:33:11 -0300

    feat: app categoryRoute

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 // ...
 const loginRoute = require('./routes/loginRoute');
 const userRoute = require('./routes/userRoute');
+const categoryRoute = require('./routes/categoryRoute');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 // ...
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
+app.use('/categories', categoryRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
