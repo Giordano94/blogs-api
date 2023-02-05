ff81089dd78b6ac83c41b185610e163491058761
Giordano <giordano.mendes94@gmail.com>
2023-02-05 16:49:25 -0300

    feat: controller getCategories

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -6,6 +6,13 @@ const insertCategory = async (req, res) => {
   return res.status(201).json(message);
 };
 
+const getCategories = async (_req, res) => {
+  const { type, message } = await categoryService.getCategories();
+  if (type) return res.status(400).json({ message });
+  res.status(200).json(message);
+};
+
 module.exports = {
   insertCategory,
+  getCategories,
 };
