b5288a48431a3e050694869d5f2e67e5f19fea6b
Giordano <giordano.mendes94@gmail.com>
2023-02-05 15:46:42 -0300

    feat: middleware validateCategory

---
+++ src/middlewares/validateCategoryName.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+  const { name } = req.body;
+
+  if (name === undefined) {
+    return res
+      .status(400)
+      .json({
+        message: '"name" is required',
+      });
+  }
+
+  next();
+};
