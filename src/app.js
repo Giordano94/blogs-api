825b03bd248ffc0b46293307830d426ac8480d77
Giordano <giordano.mendes94@gmail.com>
2023-02-05 21:40:21 -0300

    feat: app postRoute

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const loginRoute = require('./routes/loginRoute');
 const userRoute = require('./routes/userRoute');
 const categoryRoute = require('./routes/categoryRoute');
+const postRoute = require('./routes/postRouter');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
 app.use('/categories', categoryRoute);
+app.use('/post', postRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
